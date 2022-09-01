905c0e3e5c8db54e7b4c9006f1ad671a125f182e
Ayberk <ayberkdeprem@gmail.com>
2022-09-01 12:26:34 +0300

    latest version to example

--- app/src/main/java/com/deprems/fragmentjava/MainActivity.java
+++ app/src/main/java/com/deprems/fragmentjava/MainActivity.java
@@ -1,8 +1,11 @@
 package com.deprems.fragmentjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void goToFirst(View view) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout,firstFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,firstFragment).commit();
+
+
+    }
+
+    public void goToSecond(View view) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        //fragmentTransaction.add(R.id.frame_layout,secondFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,secondFragment).commit();
+
+    }
+
 }
